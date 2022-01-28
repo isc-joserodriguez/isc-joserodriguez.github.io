afe80ef010ca16061c52ba1df8984038be43bdd8
José Rodriguez <isc.joserodriguez@gmail.com>
2022-01-28 02:01:43 -0700

    Styles

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -11,11 +11,11 @@ const Projects = ({ title, projects }) => (
       <h3 className={classes.Title}>{title}</h3>
       <Row>
         {projects.map(project => {
-          return <Col key={project.id} sm='12' lg='6' className='d-flex justify-content-center align-items-center'>
-            <div className={`${classes.FlipCard} my-sm-5 my-lg-3`}>
-              <div className={classes.FlipCardInner}>
-                <div className={classes.FlipCardFront}>
-                  <Image src={project.image} alt={`${project.name}-image`} fluid />
+          return <Col key={project.id} sm='12' lg='6' className='d-flex justify-content-center align-items-center mt-3 mb-3'>
+            <div className={`${classes.FlipCard}`}>
+              <div className={`${classes.FlipCardInner}`}>
+                <div className={classes.FlipCardFront} style={{background: `url(${project.image})`, backgroundSize: '100%', backgroundRepeat: 'no-repeat'}}>
+                  {/* <Image src={project.image} alt={`${project.name}-image`} fluid /> */}
                 </div>
                 <div className={classes.FlipCardBack}>
                   <h3>{project.name}</h3>
